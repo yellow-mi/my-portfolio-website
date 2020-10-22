83a151e899ccbb7c279f24b9fa11f8db33a03981
yellow-mi <yellow-mi@users.noreply.github.com>
2020-10-22 13:45:36 +0200

    Menu button, JS & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+// Select DOM Item (Document Object Models)
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+// Create node list (like array) to create a list and select all the elements which are nav-item and are 4
+const navItems = document.querySelectorAll
+('.nav-items');
+
+// Set Initial State of Menu
+let showMenu = false;
+
+// When you click the menu button
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
